de74af4247ed2129947d937c21aac6735688e548
aaquibb <aaquib.parvez07@gmail.com>
2026-05-13 11:57:34 +0100

    vulns fixed

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False) # nosec B104
 SECRET_KEY = "hardcoded-super-secret-password-123"
