d5c0f341b27463e70a55bdf12f6e83a985b42fe9
aaquibb <aaquib.parvez07@gmail.com>
2026-05-13 11:49:19 +0100

    test: hardcodedsecret added

--- app.py
+++ app.py
@@ -18,3 +18,4 @@ def health():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
+SECRET_KEY = "hardcoded-super-secret-password-123"
